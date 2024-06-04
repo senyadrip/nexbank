9806dfb6574fc10e10d06ae422be27aea5cdd9db
senyadrip <arsenymatveev@gmail.com>
2024-06-04 00:52:50 -0400

    Hero Progress

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import styles from '../style'
+import {arrowUp} from '../assets'
 
 const GetStarted = () => {
   return (
-    <div>GetStarted</div>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full p-[2px] cursor-pointer bg-blue-gradient`}>
+      <div className={`${styles.flexCenter} flex-col bg-primary w-full h-full rounded-full`}></div>
+    </div>
   )
 }
 
