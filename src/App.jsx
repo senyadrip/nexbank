0bb607930906d76d2e147798ade75cbe6bdc3e1e
senyadrip <arsenymatveev@gmail.com>
2024-06-03 23:43:17 -0400

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,31 @@ import {Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testim
 
 const App = () => {
   return (
+    
     <div className='bg-primary w-full overflow-hidden'>
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+
+      <div className={`${styles.paddingX}${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar/>
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero/>
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonial
-          Clients
-          CTA
-          Footer
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
         </div>
       </div>
 
